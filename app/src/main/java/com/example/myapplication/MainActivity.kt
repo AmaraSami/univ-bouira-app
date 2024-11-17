3bbacf6125af619eb51b9285e08c5d132af2d7e2
AmaraSami <amarasamianis@gmail.com>
2024-11-17 12:26:58 +0100

    adding prototype of timetable page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile)) { v, insets ->
+        setContentView(R.layout.timetable)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.timetable)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
